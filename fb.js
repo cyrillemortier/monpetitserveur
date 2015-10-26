87c8062bc1814daa8879aafe03f31adb9674193c
cyrillemortier <cyrille.mortier@gmail.com>
2015-10-26 17:24:52 +0100

    initial

---
+++ fb.js
@@ -0,0 +1,6 @@
+// facebook app settings
+module.exports = {
+	'appID' : '1179820788698120',
+	'appSecret' : '',
+	'callbackUrl' : 'http://grenoble.wha.la:3000/login/facebook/callback'
+}
---
+++ twitter.js
@@ -0,0 +1,6 @@
+// twitter app settings
+module.exports = {
+	'apikey' : 'y1SidWUZXrCgshg4D1AYT1M93',
+	'apisecret' : '',
+	'callbackUrl' : 'http://grenoble.wha.la:3000/login/twitter/callback'
+}
